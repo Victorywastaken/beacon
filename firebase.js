f62516b0de7afd442fae4b9b3bf12c221ddcd59f
Victorywastaken <rlatmdfl92@gmail.com>
2021-12-08 16:02:48 -0500

    added environmental variables for keys

--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBVtw3y2bOYsPJPOmyXo-OZLuPDZfYaOxc",
-  authDomain: "beacon-e5406.firebaseapp.com",
-  projectId: "beacon-e5406",
-  storageBucket: "beacon-e5406.appspot.com",
-  messagingSenderId: "622943736613",
-  appId: "1:622943736613:web:a8c98f2fee1648449b80cc"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 let app;
